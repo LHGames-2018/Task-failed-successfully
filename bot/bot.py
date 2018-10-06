f57c1267bc6ab73b8cdb42d76be1a6c2cf3a7010
Benjamin <benjamin150594@hotmail.fr>
2018-10-06 21:43:36 +0200

    marche

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,8 @@ from helper import *
 
 class Bot:
     def __init__(self):
+        self.state = "init"
+        self.direction = Point(0,0)
         pass
 
     def before_turn(self, playerInfo):
@@ -10,6 +12,12 @@ class Bot:
         Gets called before ExecuteTurn. This is where you get your bot's state.
             :param playerInfo: Your bot's current state.
         """
+
+        if self.state == "init":
+            self.state = "searchRessource"
+        elif self.state == "searchRessource":
+            self.state = "walkRessource"
+        
         self.PlayerInfo = playerInfo
 
     def execute_turn(self, gameMap, visiblePlayers):
@@ -18,12 +26,97 @@ class Bot:
             :param gameMap: The gamemap.
             :param visiblePlayers:  The list of visible players.
         """
+        positions = self.findResource(gameMap)
+        positions = self.convertGlobalMapToLocalMap(positions)
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_move_action(Point(1, 0))
+        if (self.PlayerInfo.CarriedResources == self.PlayerInfo.CarryingCapacity):
+            if (self.PlayerInfo.HouseLocation.x < self.PlayerInfo.Position.x):
+                print("retour maison gauche")
+                return create_move_action(Point(-1, 0))
+            elif (self.PlayerInfo.HouseLocation.x > self.PlayerInfo.Position.x):
+                print("retour maison droite")
+                return create_move_action(Point(1, 0))
+            elif (self.PlayerInfo.HouseLocation.y < self.PlayerInfo.Position.y):
+                print("retour maison haut")
+                return create_move_action(Point(0, -1))
+            elif (self.PlayerInfo.HouseLocation.y > self.PlayerInfo.Position.y):
+                print("retour maison bas")
+                return create_move_action(Point(0, 1))
+        elif ((abs(positions[0].x - 10) <= 1 and positions[0].y == 10) or (abs(positions[0].y - 10) <= 1 and positions[0].x == 10)):
+            if positions[0].y < 10:
+                print("vers le haut")
+                self.direction = Point(0, -1)
+                return create_collect_action(self.direction)
+            if positions[0].y > 10:
+                print("vers le bas")
+                self.direction = Point(0, 1)
+                return create_collect_action(self.direction)
+            if positions[0].x < 10:
+                print("vers la gauche")
+                self.direction = Point(-1, 0)
+                return create_collect_action(self.direction)
+            if positions[0].x > 10:
+                print("vers la droite")
+                self.direction = Point(1, 0)
+                return create_collect_action(self.direction)
+        elif (positions[0].x < 10):
+            self.direction = Point(-1, 0)
+            return create_move_action(self.direction)
+        elif (positions[0].x > 10):
+            self.direction = Point(1, 0)
+            return create_move_action(self.direction)
+        elif (positions[0].y < 10):
+            self.direction = Point(0, -1)
+            return create_move_action(self.direction)
+        elif (positions[0].y > 10):
+            self.direction = Point(0, 1)
+            return create_move_action(self.direction)
+        return create_collect_action(self.direction)
 
     def after_turn(self):
         """
         Gets called after executeTurn
         """
         pass
+
+    def findResource(self, gameMap):
+        results = []
+        for x in range(self.PlayerInfo.Position.x - 10, self.PlayerInfo.Position.x + 10):
+            for y in range(self.PlayerInfo.Position.y - 10, self.PlayerInfo.Position.y + 10): 
+                position = Point(x, y)
+                if gameMap.getTileAt(position) == TileContent.Resource:
+                    results.append(position)
+        return self.bestPoint(results)
+
+    def bestPoint(self, points):
+        results = []
+        distances = []
+        for x in range(len(points)):
+            distances.append(Point.Distance(self.PlayerInfo.HouseLocation, points[x]))
+        while len(distances) > 0:
+            index = distances.index(min(distances))
+            results.append(points[index])
+            points.remove(points[index])
+            distances.remove(distances[index])
+        return results
+    
+    def arrayGameMap(self, gameMap):
+        results = []
+        line = []
+        for x in range(0, len(gameMap.tiles)):
+            for y in range(0, len((gameMap.tiles[0]))): 
+                position = Point(self.PlayerInfo.Position.x + x, self.PlayerInfo.Position.y + y)
+                if gameMap.getTileAt(position) == TileContent.Wall or gameMap.getTileAt(position) == TileContent.Resource:
+                    line.append("1")
+                else:
+                    line.append("0")
+            results.append(line)
+            line = []
+        return results
+
+    def convertGlobalMapToLocalMap(self, points):
+        for i in range (0, len(points)):
+            points[i].x -= (self.PlayerInfo.Position.x - 10)
+            points[i].y -= (self.PlayerInfo.Position.y - 10)
+        return points
